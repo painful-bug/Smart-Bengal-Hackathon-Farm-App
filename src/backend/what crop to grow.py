6c53b80d5d0f8dcfe93b0143886e1ac9a924eb8c
Aishikboy109 <ujanaishik109@gmail.com>
2024-03-10 12:02:25 +0530

    login page UI completed by Aishik

---
+++ src/backend/what crop to grow.py
@@ -0,0 +1,370 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from __future__ import print_function
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report
+from sklearn import metrics
+from sklearn import tree
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[2]:
+
+
+df = pd.read_csv('creco.csv')
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[4]:
+
+
+df.tail()
+
+
+# In[5]:
+
+
+df.size
+
+
+# In[6]:
+
+
+df.shape
+
+
+# In[7]:
+
+
+df.columns
+
+
+# In[8]:
+
+
+df['label'].unique()
+
+
+# In[9]:
+
+
+df.dtypes
+
+
+# In[10]:
+
+
+df['label'].value_counts()
+
+
+# In[11]:
+
+
+sns.heatmap(df.corr(),annot=True)
+
+
+# In[12]:
+
+
+features = df[['N', 'P','K','temperature', 'humidity', 'ph', 'rainfall']]
+target = df['label']
+labels = df['label']
+
+
+# In[13]:
+
+
+acc = []
+model = []
+
+
+# In[14]:
+
+
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(features,target,test_size = 0.2,random_state =2)
+
+
+# In[15]:
+
+
+from sklearn.tree import DecisionTreeClassifier
+
+DecisionTree = DecisionTreeClassifier(criterion="entropy",random_state=2,max_depth=5)
+
+DecisionTree.fit(Xtrain,Ytrain)
+
+predicted_values = DecisionTree.predict(Xtest)
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('Decision Tree')
+print("DecisionTrees's Accuracy is: ", x*100)
+
+print(classification_report(Ytest,predicted_values))
+
+
+# In[16]:
+
+
+from sklearn.model_selection import cross_val_score
+
+
+# In[17]:
+
+
+# Cross validation score (Decision Tree)
+score = cross_val_score(DecisionTree, features, target,cv=5)
+
+
+# In[18]:
+
+
+score
+
+
+# In[19]:
+
+
+import pickle
+# Dump the trained Naive Bayes classifier with Pickle
+DT_pkl_filename = 'DecisionTree.pkl'
+# Open the file to save as pkl file
+DT_Model_pkl = open(DT_pkl_filename, 'wb')
+pickle.dump(DecisionTree, DT_Model_pkl)
+# Close the pickle instances
+DT_Model_pkl.close()
+
+
+# In[20]:
+
+
+from sklearn.naive_bayes import GaussianNB
+
+NaiveBayes = GaussianNB()
+
+NaiveBayes.fit(Xtrain,Ytrain)
+
+predicted_values = NaiveBayes.predict(Xtest)
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('Naive Bayes')
+print("Naive Bayes's Accuracy is: ", x)
+
+print(classification_report(Ytest,predicted_values))
+
+
+# In[21]:
+
+
+# Cross validation score (NaiveBayes)
+score = cross_val_score(NaiveBayes,features,target,cv=5)
+score
+
+
+# In[22]:
+
+
+import pickle
+# Dump the trained Naive Bayes classifier with Pickle
+NB_pkl_filename = 'NBClassifier.pkl'
+# Open the file to save as pkl file
+NB_Model_pkl = open(NB_pkl_filename, 'wb')
+pickle.dump(NaiveBayes, NB_Model_pkl)
+# Close the pickle instances
+NB_Model_pkl.close()
+
+
+# In[23]:
+
+
+from sklearn.svm import SVC
+
+SVM = SVC(gamma='auto')
+
+SVM.fit(Xtrain,Ytrain)
+
+predicted_values = SVM.predict(Xtest)
+
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('SVM')
+print("SVM's Accuracy is: ", x)
+
+print(classification_report(Ytest,predicted_values))
+
+
+# In[24]:
+
+
+# Cross validation score (SVM)
+score = cross_val_score(SVM,features,target,cv=5)
+score
+
+
+# In[25]:
+
+
+from sklearn.linear_model import LogisticRegression
+
+LogReg = LogisticRegression(random_state=2)
+
+LogReg.fit(Xtrain,Ytrain)
+
+predicted_values = LogReg.predict(Xtest)
+
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('Logistic Regression')
+print("Logistic Regression's Accuracy is: ", x)
+
+print(classification_report(Ytest,predicted_values))
+
+
+# In[26]:
+
+
+score = cross_val_score(LogReg,features,target,cv=5)
+score
+
+
+# In[27]:
+
+
+import pickle
+# Dump the trained Naive Bayes classifier with Pickle
+LR_pkl_filename = 'LogisticRegression.pkl'
+# Open the file to save as pkl file
+LR_Model_pkl = open(DT_pkl_filename, 'wb')
+pickle.dump(LogReg, LR_Model_pkl)
+# Close the pickle instances
+LR_Model_pkl.close()
+
+
+# In[28]:
+
+
+from sklearn.ensemble import RandomForestClassifier
+
+RF = RandomForestClassifier(n_estimators=20, random_state=0)
+RF.fit(Xtrain,Ytrain)
+
+predicted_values = RF.predict(Xtest)
+
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('RF')
+print("RF's Accuracy is: ", x)
+
+print(classification_report(Ytest,predicted_values))
+
+
+# In[29]:
+
+
+# Cross validation score (Random Forest)
+score = cross_val_score(RF,features,target,cv=5)
+score
+
+
+# In[30]:
+
+
+import pickle
+# Dump the trained Naive Bayes classifier with Pickle
+RF_pkl_filename = 'RandomForest.pkl'
+# Open the file to save as pkl file
+RF_Model_pkl = open(RF_pkl_filename, 'wb')
+pickle.dump(RF, RF_Model_pkl)
+# Close the pickle instances
+RF_Model_pkl.close()
+
+
+# In[31]:
+
+
+import xgboost as xgb
+XB = xgb.XGBClassifier()
+XB.fit(Xtrain,Ytrain)
+
+predicted_values = XB.predict(Xtest)
+
+x = metrics.accuracy_score(Ytest, predicted_values)
+acc.append(x)
+model.append('XGBoost')
+print("XGBoost's Accuracy is: ", x)
+
+print(classification_report(Ytest,predicted_values))
+
+
+# In[32]:
+
+
+score = cross_val_score(XB,features,target,cv=5)
+score
+
+
+# In[33]:
+
+
+import pickle
+# Dump the trained Naive Bayes classifier with Pickle
+XB_pkl_filename = 'XGBoost.pkl'
+# Open the file to save as pkl file
+XB_Model_pkl = open(XB_pkl_filename, 'wb')
+pickle.dump(XB, XB_Model_pkl)
+# Close the pickle instances
+XB_Model_pkl.close()
+
+
+# In[34]:
+
+
+plt.figure(figsize=[10,5],dpi = 100)
+plt.title('Accuracy Comparison')
+plt.xlabel('Accuracy')
+plt.ylabel('Algorithm')
+sns.barplot(x = acc,y = model,palette='dark')
+
+
+# In[35]:
+
+
+accuracy_models = dict(zip(model, acc))
+for k, v in accuracy_models.items():
+    print (k, '-->', v)
+
+
+# In[36]:
+
+
+data = np.array([[104,18, 30, 23.603016, 60.3, 6.7, 140.91]])
+prediction = RF.predict(data)
+print(prediction)
+
+
+# In[37]:
+
+
+data = np.array([[83, 45, 60, 28, 70.3, 7.0, 150.9]])
+prediction = RF.predict(data)
+print(prediction)
+
+
+# In[ ]:
+
+
+
+
--- src/frontend/components/LoginPage.js
+++ src/frontend/components/LoginPage.js
@@ -1,37 +1,54 @@
-import React from 'react'
-import '../styles/LoginPage.css';
+import React from "react";
+import "../styles/LoginPage.css";
 
 import { MdEmail } from "react-icons/md";
 import { RiLockPasswordFill } from "react-icons/ri";
+import Button from '@mui/material/Button';
 
 export const LoginPage = () => {
   return (
     <div className="login-page">
-        <h1>Login</h1>
-        <form action="">
-            <div className="input-box">
-                <input type="email" placeholder="Email" name="email" id="email" required/>
-                <MdEmail className="icon"/>
-            </div>
+      <h1>Login</h1>
+      <form action="">
+        <div className="input-box">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            id="email"
+            required
+          />
+          <MdEmail className="icon" />
+        </div>
 
-            <div className="input-box">
-                <input type="password" placeholder="Password" name="password" id="password" required/>
-                <RiLockPasswordFill className="icon" />
-            </div>
+        <div className="input-box">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            id="password"
+            required
+          />
+          <RiLockPasswordFill className="icon" />
+        </div>
 
-            <div className="remember-me">
-                <input type="checkbox" name="remember-me" id="remember-me" />
-                <label htmlFor="remember-me">Remember me</label>
-                <a href="#">Forgot password?</a>
-            </div>
+        <div className="remember-me">
+          <div className="remember-me-check">
+            <input type="checkbox" name="remember-me" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <a href="#">Forgot password?</a>
+        </div>
 
-            <div className="login-button">
-                <button type="submit">Login</button>
-            </div>
-            <div className="register-button">
-                <p>Don't have an account? <a href="#">Register</a></p>
-            </div>
-        </form>
+        <div className="login-button">
+          <button type="submit">Login</button>
+        </div>
+        <div className="register-button">
+          <p>
+            Don't have an account? <a href="#">Register</a>
+          </p>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
